c34f20da4d2b7038d19d9dfd40708c010416632e
Aditya <akkpk933@gmail.com>
2023-12-03 10:44:10 +0530

    change for-loop to for-each loop

--- todo-list.js
+++ todo-list.js
@@ -5,22 +5,19 @@ renderTodoList();
 function renderTodoList() {
   let todoListHTML = "";
 
-  for (let i = 0; i < todoList.length; i++) {
-    const todoObject = todoList[i];
-    // const name = todoObject.name;
-    // const dueDate = todoObject.dueDate;
+  todoList.forEach(function(todoObject, index){
     const { name,dueDate } = todoObject;
     const html = `
       <div>${name}</div>
       <div>${dueDate}</div>
       <button onClick="
-        todoList.splice(${i}, 1);
+        todoList.splice(${index}, 1);
         renderTodoList();
       " class="delete-todo-button">Delete</button>
       
     `;
     todoListHTML += html;
-  }
+  });
   
   document.querySelector(".js-todo-list").innerHTML = todoListHTML;
 }
