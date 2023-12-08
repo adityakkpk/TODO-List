5f4e52140f6c1e8e7819a1d1f70d159fd79ea76d
Aditya <akkpk933@gmail.com>
2023-12-08 10:37:10 +0530

    checkbox added

--- todo-list.js
+++ todo-list.js
@@ -1,4 +1,4 @@
-let todoList = [];
+let todoList = JSON.parse(localStorage.getItem('todoList')) || [];
 
 renderTodoList();
 
@@ -7,30 +7,17 @@ function renderTodoList() {
 
   todoList.forEach((todoObject, index) => {
     const { name,dueDate } = todoObject;
-    const html = `
-      <div>${name}</div>
+    const html = ` 
+      <div>
+        <input type="checkbox">
+        ${name}
+      </div>
       <div>${dueDate}</div>
       <button class="delete-todo-button js-delete-todo-button">Delete</button>
       
     `;
     todoListHTML += html;
   });
-  // for (let i = 0; i < todoList.length; i++) {
-  //   const todoObject = todoList[i];
-  //   // const name = todoObject.name;
-  //   // const dueDate = todoObject.dueDate;
-  //   const { name,dueDate } = todoObject;
-  //   const html = `
-  //     <div>${name}</div>
-  //     <div>${dueDate}</div>
-  //     <button onClick="
-  //       todoList.splice(${i}, 1);
-  //       renderTodoList();
-  //     " class="delete-todo-button">Delete</button>
-      
-  //   `;
-  //   todoListHTML += html;
-  // }
   
   document.querySelector(".js-todo-list").innerHTML = todoListHTML;
 
@@ -38,6 +25,7 @@ function renderTodoList() {
     .forEach((deleteButton, index)=> {
       deleteButton.addEventListener('click', () => {
         todoList.splice(index, 1);
+        localStorage.setItem('todoList', JSON.stringify(todoList));
         renderTodoList();
       });
     });
@@ -55,11 +43,11 @@ function addTodo() {
   const dueDate = dateInputElement.value;
 
   todoList.push({
-    // name: name,
-    // dueDate: dueDate
     name,
     dueDate
   });
+
+  localStorage.setItem('todoList', JSON.stringify(todoList));
 
   inputElement.value = "";
 
